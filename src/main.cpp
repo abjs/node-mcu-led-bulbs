6d7ac5194a3c69d0a7c47d3628392135a8be60a4
Abin Joseph <itsmeabjs@gmail.com>
2021-12-01 01:47:47 +0530

    3

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,15 @@
 #define H HIGH
 #define L LOW
 #define LED_0 LED_BUILTIN
-#define LED_1 D0
-#define LED_2 D1
-#define LED_3 D2
-#define LED_4 D3
-#define LED_5 D4
-#define LED_6 D5
-#define LED_7 D6
-#define LED_8 D7
-#define LED_9 D8
+#define LED_1 D8
+#define LED_2 D0
+#define LED_3 D1
+#define LED_4 D2
+#define LED_5 D3
+#define LED_6 D4
+#define LED_7 D5
+#define LED_8 D6
+#define LED_9 D7
 void setup()
 {
   pinMode(LED_0, OUTPUT);
@@ -116,5 +116,61 @@ void loop()
   digitalWrite(LED_7, L);
   digitalWrite(LED_8, L);
   digitalWrite(LED_9, H);
+  delay(500);
+  digitalWrite(LED_1, H);
+  digitalWrite(LED_2, H);
+  digitalWrite(LED_3, H);
+  digitalWrite(LED_4, H);
+  digitalWrite(LED_5, H);
+  digitalWrite(LED_6, H);
+  digitalWrite(LED_7, H);
+  digitalWrite(LED_8, H);
+  digitalWrite(LED_9, H);
+  delay(500);
+  digitalWrite(LED_1, L);
+  digitalWrite(LED_2, L);
+  digitalWrite(LED_3, L);
+  digitalWrite(LED_4, L);
+  digitalWrite(LED_5, L);
+  digitalWrite(LED_6, L);
+  digitalWrite(LED_7, L);
+  digitalWrite(LED_8, L);
+  digitalWrite(LED_9, L);
+  delay(500);
+  digitalWrite(LED_1, H);
+  delay(500);
+  digitalWrite(LED_2, H);
+  delay(500);
+  digitalWrite(LED_3, H);
+  delay(500);
+  digitalWrite(LED_4, H);
+  delay(500);
+  digitalWrite(LED_5, H);
+  delay(500);
+  digitalWrite(LED_6, H);
+  delay(500);
+  digitalWrite(LED_7, H);
+  delay(500);
+  digitalWrite(LED_8, H);
+  delay(500);
+  digitalWrite(LED_9, H);
+  delay(500);
+  digitalWrite(LED_1, L);
+  delay(500);
+  digitalWrite(LED_2, L);
+  delay(500);
+  digitalWrite(LED_3, L);
+  delay(500);
+  digitalWrite(LED_4, L);
+  delay(500);
+  digitalWrite(LED_5, L);
+  delay(500);
+  digitalWrite(LED_6, L);
+  delay(500);
+  digitalWrite(LED_7, L);
+  delay(500);
+  digitalWrite(LED_8, L);
+  delay(500);
+  digitalWrite(LED_9, L);
   delay(500);
 }
