05fde87ef04886d1cc5a09452c90fa2018263337
Abin Joseph <itsmeabjs@gmail.com>
2021-12-01 01:37:04 +0530

    led series

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,120 @@
 #include <Arduino.h>
-
-void setup() {
-  // put your setup code here, to run once:
+#define H HIGH
+#define L LOW
+#define LED_0 LED_BUILTIN
+#define LED_1 D0
+#define LED_2 D1
+#define LED_3 D2
+#define LED_4 D3
+#define LED_5 D4
+#define LED_6 D5
+#define LED_7 D6
+#define LED_8 D7
+#define LED_9 D8
+void setup()
+{
+  pinMode(LED_0, OUTPUT);
+  pinMode(LED_1, OUTPUT);
+  pinMode(LED_2, OUTPUT);
+  pinMode(LED_3, OUTPUT);
+  pinMode(LED_4, OUTPUT);
+  pinMode(LED_5, OUTPUT);
+  pinMode(LED_6, OUTPUT);
+  pinMode(LED_7, OUTPUT);
+  pinMode(LED_8, OUTPUT);
+  pinMode(LED_9, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  digitalWrite(LED_1, H);
+  digitalWrite(LED_2, L);
+  digitalWrite(LED_3, L);
+  digitalWrite(LED_4, L);
+  digitalWrite(LED_5, L);
+  digitalWrite(LED_6, L);
+  digitalWrite(LED_7, L);
+  digitalWrite(LED_8, L);
+  digitalWrite(LED_9, L);
+  delay(500);
+  digitalWrite(LED_1, L);
+  digitalWrite(LED_2, H);
+  digitalWrite(LED_3, L);
+  digitalWrite(LED_4, L);
+  digitalWrite(LED_5, L);
+  digitalWrite(LED_6, L);
+  digitalWrite(LED_7, L);
+  digitalWrite(LED_8, L);
+  digitalWrite(LED_9, L);
+  delay(500);
+  digitalWrite(LED_1, L);
+  digitalWrite(LED_2, L);
+  digitalWrite(LED_3, H);
+  digitalWrite(LED_4, L);
+  digitalWrite(LED_5, L);
+  digitalWrite(LED_6, L);
+  digitalWrite(LED_7, L);
+  digitalWrite(LED_8, L);
+  digitalWrite(LED_9, L);
+  delay(500);
+  digitalWrite(LED_1, L);
+  digitalWrite(LED_2, L);
+  digitalWrite(LED_3, L);
+  digitalWrite(LED_4, H);
+  digitalWrite(LED_5, L);
+  digitalWrite(LED_6, L);
+  digitalWrite(LED_7, L);
+  digitalWrite(LED_8, L);
+  digitalWrite(LED_9, L);
+  delay(500);
+  digitalWrite(LED_1, L);
+  digitalWrite(LED_2, L);
+  digitalWrite(LED_3, L);
+  digitalWrite(LED_4, L);
+  digitalWrite(LED_5, H);
+  digitalWrite(LED_6, L);
+  digitalWrite(LED_7, L);
+  digitalWrite(LED_8, L);
+  digitalWrite(LED_9, L);
+  delay(500);
+  digitalWrite(LED_1, L);
+  digitalWrite(LED_2, L);
+  digitalWrite(LED_3, L);
+  digitalWrite(LED_4, L);
+  digitalWrite(LED_5, L);
+  digitalWrite(LED_6, H);
+  digitalWrite(LED_7, L);
+  digitalWrite(LED_8, L);
+  digitalWrite(LED_9, L);
+  delay(500);
+  digitalWrite(LED_1, L);
+  digitalWrite(LED_2, L);
+  digitalWrite(LED_3, L);
+  digitalWrite(LED_4, L);
+  digitalWrite(LED_5, L);
+  digitalWrite(LED_6, L);
+  digitalWrite(LED_7, H);
+  digitalWrite(LED_8, L);
+  digitalWrite(LED_9, L);
+  delay(500);
+  digitalWrite(LED_1, L);
+  digitalWrite(LED_2, L);
+  digitalWrite(LED_3, L);
+  digitalWrite(LED_4, L);
+  digitalWrite(LED_5, L);
+  digitalWrite(LED_6, L);
+  digitalWrite(LED_7, L);
+  digitalWrite(LED_8, H);
+  digitalWrite(LED_9, L);
+  delay(500);
+  digitalWrite(LED_1, L);
+  digitalWrite(LED_2, L);
+  digitalWrite(LED_3, L);
+  digitalWrite(LED_4, L);
+  digitalWrite(LED_5, L);
+  digitalWrite(LED_6, L);
+  digitalWrite(LED_7, L);
+  digitalWrite(LED_8, L);
+  digitalWrite(LED_9, H);
+  delay(500);
 }
