454c14e8f942997f8018261cd0ce954b33c101bc
Abin  Joseph <itsmeabjs@gmail.com>
2021-12-01 08:38:19 +0530

    start

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,7 @@
 #include <Arduino.h>
 void setup()
 {
-  pinMode(D0, OUTPUT);
-  pinMode(D1, OUTPUT);
-  pinMode(D2, OUTPUT);
-  pinMode(D3, OUTPUT);
-  pinMode(D4, OUTPUT);
-  pinMode(D5, OUTPUT);
-  pinMode(D6, OUTPUT);
-  pinMode(D7, OUTPUT);
-  pinMode(D8, OUTPUT);
-  digitalWrite(D0, HIGH);
-  digitalWrite(D1, HIGH);
-  digitalWrite(D2, HIGH);
-  digitalWrite(D3, HIGH);
-  digitalWrite(D4, HIGH);
-  digitalWrite(D5, HIGH);
-  digitalWrite(D6, HIGH);
-  digitalWrite(D7, HIGH);
-  digitalWrite(D8, HIGH);
+
 }
 
 void loop()
