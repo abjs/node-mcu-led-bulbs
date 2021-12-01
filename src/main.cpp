a701d0bea907c8463586204c5d1166c5c7d5827b
Abin Joseph <itsmeabjs@gmail.com>
2021-12-01 12:29:19 +0530

    lcd

--- src/main.cpp
+++ src/main.cpp
@@ -1,196 +1,32 @@
 #include <Arduino.h>
-#define H HIGH
-#define L LOW
-#define LED_0 LED_BUILTIN
-#define LED_1 D0
-#define LED_2 D1
-#define LED_3 D2
-#define LED_4 D3
-#define LED_5 D4
-#define LED_6 D5
-#define LED_7 D6
-#define LED_8 D7
-#define LED_9 D8
+#include <LiquidCrystal_I2C.h>
+int lcdColumns = 16;
+int lcdRows = 4;
+LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows);
+void setCursor(int x, int y)
+{
+  if (y >= 2)
+  {
+    x -= 4;
+  }
+  lcd.setCursor(x, y);
+}
+void lcdPrint(int x, int y, const char message[])
+{
+  setCursor(x, y);
+  lcd.print(message);
+}
 void setup()
 {
-  pinMode(LED_0, OUTPUT);
-  pinMode(LED_1, OUTPUT);
-  pinMode(LED_2, OUTPUT);
-  pinMode(LED_3, OUTPUT);
-  pinMode(LED_4, OUTPUT);
-  pinMode(LED_5, OUTPUT);
-  pinMode(LED_6, OUTPUT);
-  pinMode(LED_7, OUTPUT);
-  pinMode(LED_8, OUTPUT);
-  pinMode(LED_9, OUTPUT);
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
+  lcdPrint(4, 0, "Hi ABIN");
+  lcdPrint(3, 1, "Welcome To");
+  lcdPrint(3, 2, "Hack Club");
+  lcdPrint(6, 3, "KMEA");
 }
 
 void loop()
 {
-  digitalWrite(LED_1, H);
-  digitalWrite(LED_2, L);
-  digitalWrite(LED_3, L);
-  digitalWrite(LED_4, L);
-  digitalWrite(LED_5, L);
-  digitalWrite(LED_6, L);
-  digitalWrite(LED_7, L);
-  digitalWrite(LED_8, L);
-  digitalWrite(LED_9, L);
-  delay(1000);
-  digitalWrite(LED_1, L);
-  digitalWrite(LED_2, H);
-  digitalWrite(LED_3, L);
-  digitalWrite(LED_4, L);
-  digitalWrite(LED_5, L);
-  digitalWrite(LED_6, L);
-  digitalWrite(LED_7, L);
-  digitalWrite(LED_8, L);
-  digitalWrite(LED_9, L);
-  delay(1000);
-  digitalWrite(LED_1, L);
-  digitalWrite(LED_2, L);
-  digitalWrite(LED_3, H);
-  digitalWrite(LED_4, L);
-  digitalWrite(LED_5, L);
-  digitalWrite(LED_6, L);
-  digitalWrite(LED_7, L);
-  digitalWrite(LED_8, L);
-  digitalWrite(LED_9, L);
-  delay(1000);
-  digitalWrite(LED_1, L);
-  digitalWrite(LED_2, L);
-  digitalWrite(LED_3, L);
-  digitalWrite(LED_4, H);
-  digitalWrite(LED_5, L);
-  digitalWrite(LED_6, L);
-  digitalWrite(LED_7, L);
-  digitalWrite(LED_8, L);
-  digitalWrite(LED_9, L);
-  delay(1000);
-  digitalWrite(LED_1, L);
-  digitalWrite(LED_2, L);
-  digitalWrite(LED_3, L);
-  digitalWrite(LED_4, L);
-  digitalWrite(LED_5, H);
-  digitalWrite(LED_6, L);
-  digitalWrite(LED_7, L);
-  digitalWrite(LED_8, L);
-  digitalWrite(LED_9, L);
-  delay(1000);
-  digitalWrite(LED_1, L);
-  digitalWrite(LED_2, L);
-  digitalWrite(LED_3, L);
-  digitalWrite(LED_4, L);
-  digitalWrite(LED_5, L);
-  digitalWrite(LED_6, H);
-  digitalWrite(LED_7, L);
-  digitalWrite(LED_8, L);
-  digitalWrite(LED_9, L);
-  delay(1000);
-  digitalWrite(LED_1, L);
-  digitalWrite(LED_2, L);
-  digitalWrite(LED_3, L);
-  digitalWrite(LED_4, L);
-  digitalWrite(LED_5, L);
-  digitalWrite(LED_6, L);
-  digitalWrite(LED_7, H);
-  digitalWrite(LED_8, L);
-  digitalWrite(LED_9, L);
-  delay(1000);
-  digitalWrite(LED_1, L);
-  digitalWrite(LED_2, L);
-  digitalWrite(LED_3, L);
-  digitalWrite(LED_4, L);
-  digitalWrite(LED_5, L);
-  digitalWrite(LED_6, L);
-  digitalWrite(LED_7, L);
-  digitalWrite(LED_8, H);
-  digitalWrite(LED_9, L);
-  delay(1000);
-  digitalWrite(LED_1, L);
-  digitalWrite(LED_2, L);
-  digitalWrite(LED_3, L);
-  digitalWrite(LED_4, L);
-  digitalWrite(LED_5, L);
-  digitalWrite(LED_6, L);
-  digitalWrite(LED_7, L);
-  digitalWrite(LED_8, L);
-  digitalWrite(LED_9, H);
-  delay(1000);
-  digitalWrite(LED_1, H);
-  digitalWrite(LED_2, H);
-  digitalWrite(LED_3, H);
-  digitalWrite(LED_4, H);
-  digitalWrite(LED_5, H);
-  digitalWrite(LED_6, H);
-  digitalWrite(LED_7, H);
-  digitalWrite(LED_8, H);
-  digitalWrite(LED_9, H);
-  delay(1000);
-  digitalWrite(LED_1, L);
-  digitalWrite(LED_2, L);
-  digitalWrite(LED_3, L);
-  digitalWrite(LED_4, L);
-  digitalWrite(LED_5, L);
-  digitalWrite(LED_6, L);
-  digitalWrite(LED_7, L);
-  digitalWrite(LED_8, L);
-  digitalWrite(LED_9, L);
-  delay(1000);
-  digitalWrite(LED_1, H);
-  delay(1000);
-  digitalWrite(LED_2, H);
-  delay(1000);
-  digitalWrite(LED_3, H);
-  delay(1000);
-  digitalWrite(LED_4, H);
-  delay(1000);
-  digitalWrite(LED_5, H);
-  delay(1000);
-  digitalWrite(LED_6, H);
-  delay(1000);
-  digitalWrite(LED_7, H);
-  delay(1000);
-  digitalWrite(LED_8, H);
-  delay(1000);
-  digitalWrite(LED_9, H);
-  delay(1000);
-  digitalWrite(LED_1, L);
-  delay(1000);
-  digitalWrite(LED_2, L);
-  delay(1000);
-  digitalWrite(LED_3, L);
-  delay(1000);
-  digitalWrite(LED_4, L);
-  delay(1000);
-  digitalWrite(LED_5, L);
-  delay(1000);
-  digitalWrite(LED_6, L);
-  delay(1000);
-  digitalWrite(LED_7, L);
-  delay(1000);
-  digitalWrite(LED_8, L);
-  delay(1000);
-  digitalWrite(LED_9, L);
-  delay(1000);
-  digitalWrite(LED_1, H);
-  digitalWrite(LED_2, L);
-  digitalWrite(LED_3, H);
-  digitalWrite(LED_4, L);
-  digitalWrite(LED_5, H);
-  digitalWrite(LED_6, L);
-  digitalWrite(LED_7, H);
-  digitalWrite(LED_8, L);
-  digitalWrite(LED_9, H);
-  delay(1000);
-  digitalWrite(LED_1, L);
-  digitalWrite(LED_2, H);
-  digitalWrite(LED_3, L);
-  digitalWrite(LED_4, H);
-  digitalWrite(LED_5, L);
-  digitalWrite(LED_6, H);
-  digitalWrite(LED_7, L);
-  digitalWrite(LED_8, H);
-  digitalWrite(LED_9, L);
-  delay(1000);
 }
